8bd12e5a96e3b3881e24a4562b62bec904db17aa
Georgi Cholakov <g.cholakov@nursit-institute.de>
2024-05-08 20:10:41 +0300

    test

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+        type="radio"
+        :checked="modelVAlue === value"
+        :value="value"
+        @change="$emit('update:modelValue', value)"
+        v-bind="$attrs"
+    />
+    <label v-if="label">{{label}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelVAlue: {
+      type: [String, Number],
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/views/SimpleForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form @submit.prevent="sendForm">
+      <base-select
+        :options="categories"
+        v-model="event.category"
+        label="Select a category"
+      />
+
+      <h3>Name & describe your event</h3>
+
+      <BaseInput
+      v-model="event.title"
+      label="Title"
+      type="text"
+      />
+
+      <BaseInput
+      v-model="event.description"
+      label="Description"
+      type="text"
+      />
+
+      <h3>Where is your event?</h3>
+
+      <BaseInput v-model="event.location"
+      label="Location"
+      type="text" />
+
+      <h3>Are pets allowed?</h3>
+      <div>
+        <BaseRadioGroup
+          v-model="event.pets"
+          name="pets"
+          :options="petOptions"
+          vertical
+        />
+      </div>
+      <h3>Extras</h3>
+      <div>
+        <BaseCheckbox
+        v-model="event.extras.catering"
+        label="Catering"
+        />
+      </div>
+
+      <div>
+        <BaseCheckbox
+        v-model="event.extras.music"
+        label="Live music"
+        />
+      </div>
+
+      <button
+      class="button -fill-gradient"
+      type="submit"
+      >Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfar',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ],
+      event: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false
+        }
+      },
+      petOptions: [
+        { label: 'Yes', value: 1 },
+        { label: 'No', value: 'No' }
+      ]
+    }
+  },
+  methods: {
+    sendForm () {
+      //
+    }
+  }
+}
+</script>
